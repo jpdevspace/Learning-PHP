a01edc2a76965f9d29ff603e273339e97a2e985c
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-04 23:55:10 -0600

    Connecting to my local instance of MySQL and fetching data

--- 02 Dynamic Web/index.php
+++ 02 Dynamic Web/index.php
@@ -6,8 +6,21 @@ require('functions.php');
 // require('router.php');
 
 // Connect to our MySQL DB
+$dsn = 'mysql:host=localhost;port=3306;dbname=laracast_myapp;user=root;charset=utf8mb4';
+
+$pdo = new PDO($dsn);
 
 
+$statement = $pdo->prepare('SELECT * FROM posts');
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
+
+// THIS EXAMPLE HAS NOTHING TO DO WITH THE APP, IT'S JUST 
+// NOTES, THIS IS HOW YOU CREATE A CLASS, INSTANTIATE IT AND
+// CALL ITS FIELDS AND METHODS WITH -> 
 class Person 
 {
   public $name;
@@ -23,4 +36,4 @@ $person = new Person();
 
 $person->name ='John Doe';
 $person->age = 37;
-$person->breathe(); // John Doe is breathing
+// $person->breathe(); // John Doe is breathing

99153225848566d614ce916cd12ec680995f957f
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-09 00:13:31 -0600

    Intro to validating forms

--- Notes App/controllers/note-create.php
+++ Notes App/controllers/note-create.php
@@ -7,8 +7,19 @@ $heading = "Create New Note";
 $REQ_METHOD = $_SERVER["REQUEST_METHOD"];
 
 if ($REQ_METHOD === "POST") {
+  $errors = [];
+  $body = $_POST["body"];
+
+  if (strlen($body) === 0) {
+    $errors["body"] = "A body is required";
+  }
+
+  if (strlen($body) > 1000) {
+    $errors["body"] = "Body can't be longer than 1,000 characters";
+  }
+
   $db->query("INSERT INTO notes (body, user_id) VALUES(:body, :user_id)", [
-    "body" => $_POST["body"],
+    "body" => $body,
     "user_id" => 1
   ]);
   // dd($form_input);

2f829a2264fa6a611c670d62107c37c2cc37b072
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-10 14:32:15 -0600

    example with :: operator to call static methods

--- Notes App/public/index.php
+++ Notes App/public/index.php
@@ -21,28 +21,3 @@ spl_autoload_register(function ($class) {
 // require base_path("Response.php");
 
 require base_path("router.php");
-
-/*
-|--------------------------------------------------------------------------
-| CLASS EXAMPLE
-|--------------------------------------------------------------------------
-| This example has nothing to do with the app
-| but it shows how to create a class, fields and methods
-| and how to call them with ->
-*/
-class Person
-{
-  public $name;
-  public $age;
-
-  public function breathe()
-  {
-    echo $this->name . " is breathing";
-  }
-}
-
-$person = new Person();
-
-$person->name = "John Doe";
-$person->age = 37;
-// $person->breathe(); // John Doe is breathing

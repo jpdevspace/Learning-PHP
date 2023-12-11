ae9983ea33497b7be6ca2adc57e2ff5e265bf959
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-11 15:08:10 -0600

    setup email notifications when a new chirp is created

---
+++ Laravel/chirper/app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        /**
+         * All users (except the author) will be notified that a new Chirp has been created
+         */
+        foreach(User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
